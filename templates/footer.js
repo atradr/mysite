c261f160acd8144498b62b28b845fc748f4a8649
Austin Rogers <austintradr@gmail.com>
2017-09-24 21:49:49 -0400

    Organize code, add js templates, make a few style changes

---
+++ templates/footer.js
@@ -0,0 +1,19 @@
+document.write('\
+\
+<footer>\
+    <div class="contact">\
+        <a href="https://twitter.com/austintradr" target="_blank">\
+            <img src="images/twitterbird.png"/>\
+        </a>\
+\
+        <a href="mailto:austin@austintradrogers.com?Subject=Contacing%20from%20site">\
+            <img src="images/email-icon.png"/>\
+        </a>\
+\
+        <a href="https://github.com/atradr" target="_blank">\
+            <img src="images/github-icon.png" />\
+        </a>\
+    </div>\
+</footer>\
+\
+');
---
+++ templates/header.js
@@ -0,0 +1,17 @@
+document.write('\
+\
+<header>\
+    <nav>\
+        <a id="logolink" href="http://www.atradr.com">\
+            <img id="logo" src="images/logo.png" alt="Austin Trad Rogers" title="Austin Trad Rogers">\
+        </a>\
+        <div>\
+            <ul>\
+                <li>about me</li>\
+            </ul>\
+        </div>\
+        <p id="intro">hello i\'m austin rogers</p>\
+    </nav>\
+</header>\
+\
+');
